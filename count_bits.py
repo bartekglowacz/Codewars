98101afa4b4dae04cf4b465ae8205d48eb1f0d59
bartekglowacz <bartekglowacz@gmail.com>
2023-02-04 15:13:17 +0100

    ok

---
+++ count_bits.py
@@ -0,0 +1,22 @@
+import random
+
+
+def count_bits(decimal_form):
+    binary_form = bin(decimal_form).removeprefix("0b")
+    print(binary_form)
+    suma = 0
+    if decimal_form >= 0:
+        for x in binary_form:
+            if x == "1":
+                suma += 1
+        print(f"suma bitowa: {suma}")
+        return suma
+    else:
+        print("input should be a non-negative integer")
+        return 0
+
+
+number = random.randint(-9, 9) * random.randint(-9, 99)
+print(number)
+
+count_bits(number)
---
+++ even_or_odd.py
@@ -0,0 +1,13 @@
+import random
+
+
+def even_or_odd(numb):
+    if numb % 2 == 0:
+        return print("Even")
+    if numb % 2 != 0:
+        return print("Odd")
+
+
+number = random.randint(-9, 9) * random.randint(-9, 9)
+print(number)
+even_or_odd(number)
